6f8a9ea47340677ffd82d9f410f2c59fbaecddd2
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-12-21 22:23:05 +0300

    introduction to konya map

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useEffect } from "react";
+import { MapContainer } from "react-leaflet/MapContainer";
+import { TileLayer } from "react-leaflet/TileLayer";
+import "leaflet/dist/leaflet.css";
+import { useMap } from "react-leaflet/hooks";
+import L, { latLng, latLngBounds } from "leaflet";
 
 const Map = () => {
+  const mapBoundaries = {
+    southWest: latLng(34.025514, 25.584519),
+    northEast: latLng(42.211024, 44.823563),
+  };
+
+  const bounds = latLngBounds(mapBoundaries.southWest, mapBoundaries.northEast);
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="w-full h-[100vh] flex flex-col justify-center items-center gap-5">
+      <MapContainer
+        center={[39.1560158, 35.0479979]}
+        zoom={7}
+        scrollWheelZoom={true}
+        className="w-full h-full"
+        zoomSnap={1}
+        zoomDelta={1}
+        maxBounds={bounds}
+        maxZoom={18}
+        minZoom={7}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Konya />
+      </MapContainer>
+    </div>
   );
+};
+
+const Konya = () => {
+  const map = useMap();
+  function drawCountyBoundary() {
+    const url = `https://nominatim.openstreetmap.org/search.php?state=Konya&polygon_geojson=1&format=jsonv2`;
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        const geojsonFeature = json[0].geojson;
+        L.geoJSON(geojsonFeature)
+          .on("click", (e) => {
+            map.fitBounds(e.target.getBounds(), {
+              animate: true,
+            });
+          })
+          .addTo(map);
+      });
+  }
+
+  drawCountyBoundary();
 };
 
 export default Map;
