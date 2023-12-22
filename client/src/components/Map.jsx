36789eb9bc184893d57ac5794ac5881bc5b01a6a
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-12-22 22:26:17 +0300

    continue to map handling

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer';
 import { TileLayer } from 'react-leaflet/TileLayer';
 import 'leaflet/dist/leaflet.css';
 import { useMap } from 'react-leaflet/hooks';
 import L, { latLng, latLngBounds } from 'leaflet';
+import { useState } from 'react';
 
 const Map = () => {
-  const [selectedCity, setSelectedCity] = useState(null);
-
+  const [test, setTest] = useState(false);
   const mapBoundaries = {
     southWest: latLng(34.025514, 25.584519),
     northEast: latLng(42.211024, 44.823563),
@@ -20,86 +19,141 @@ const Map = () => {
         center={[39.1560158, 35.0479979]}
         zoom={7}
         scrollWheelZoom={true}
-        className="w-full h-full"
+        className="w-full h-full z-10"
         zoomSnap={1}
         zoomDelta={1}
         maxBounds={bounds}
         maxZoom={18}
         minZoom={7}
       >
+        <Cities />
         <TileLayer
           attribution="418 Teapot MEDAŞ Askerleri"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {selectedCity ? <Counties /> : null}
-
-        <Cities setSelectedCity={setSelectedCity} />
-        {/* <KonyaElektrikIstasyonlari /> */}
-        {/* <KonyaStations /> */}
       </MapContainer>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded absolute top-10 left-5 z-20"
+        onClick={() => {
+          setTest(!test);
+        }}
+      >
+        Reset Map
+      </button>
     </div>
   );
 };
 
-const getRandomColor = () => {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+const Cities = () => {
+  const map = useMap();
+  drawCityBoundary(map);
 };
 
-// const Cities = () => {
-//   const map = useMap();
-//   function drawCountyBoundary(city) {
-//     const url = `https://nominatim.openstreetmap.org/search.php?state=${city}&polygon_geojson=1&format=jsonv2`;
-//     fetch(url)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (json) {
-//         const geojsonFeature = json[0].geojson;
-//         L.geoJSON(geojsonFeature, {
-//           style: {
-//             color: getRandomColor(),
-//           },
-//         })
-//           .on("click", (e) => {
-//             map.fitBounds(e.target.getBounds(), {
-//               animate: true,
-//             });
-//           })
-//           .addTo(map);
-//       });
-//   }
-
-//   cities.forEach((city) => {
-//     drawCountyBoundary(city);
-//   });
-// };
-
-const KonyaStations = () => {
+const resetMap = () => {
   const map = useMap();
-  function pinStations() {
-    fetch('../../constants/benzin-istasyonlari.geojson')
-      .then(function (response) {
-        return response.json();
-      })
-      .then((json) => {
-        const array = json.features;
-        for (let index = 0; index < array.length; index++) {
-          const element = array[index];
-          L.marker(element.geometry.coordinates).addTo(map);
-        }
-      });
-  }
-
-  pinStations();
+  map.eachLayer((layer) => {
+    if (layer instanceof L.GeoJSON) {
+      map.removeLayer(layer);
+    }
+  });
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+  });
+  drawCityBoundary(map);
 };
 
-const KonyaElektrikIstasyonlari = () => {
-  const map = useMap();
+function drawCityBoundary(map) {
+  fetch('../../constants/cities.geojson')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const array = json.features;
+      const featureGroup = [];
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+        const geoJson = L.geoJSON(element.geometry, {
+          title: element?.properties?.name,
+          style: {
+            color: 'black',
+            fillColor: 'lightgray',
+            weight: 2,
+          },
+        })
+          .on('click', (e) => {
+            map.fitBounds(e.target.getBounds(), {
+              animate: true,
+            });
+            map.eachLayer((layer) => {
+              if (layer instanceof L.GeoJSON) {
+                map.removeLayer(layer);
+              }
+            });
+            drawCountyBoundary(map, element?.properties?.name);
+          })
+          .on('mouseover', (e) => {
+            e.target.setStyle({
+              fillColor: 'red',
+            });
+          })
+          .on('mouseout', (e) => {
+            e.target.setStyle({
+              fillColor: 'lightgray',
+            });
+          });
+        featureGroup.push(geoJson);
+      }
+      L.featureGroup(featureGroup).addTo(map);
+    });
+}
+
+function drawCountyBoundary(map, cityName) {
+  fetch(`../../constants/${cityName}_ilceler.geojson`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const array = json.features;
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        L.geoJSON(element.geometry, {
+          style: {
+            color: 'black',
+            fillColor: 'blue',
+            weight: 1,
+          },
+        })
+          .on('click', (e) => {
+            map.fitBounds(e.target.getBounds(), {
+              animate: true,
+            });
+            map.eachLayer((layer) => {
+              // TODO: test this
+              if (layer instanceof L.Marker) {
+                map.removeLayer(layer);
+              }
+            });
+            pinMarkers(map, 'Karatay');
+          })
+          .on('mouseover', (e) => {
+            e.target.setStyle({
+              fillColor: 'red',
+            });
+          })
+          .on('mouseout', (e) => {
+            e.target.setStyle({
+              fillColor: 'blue',
+            });
+          })
+          .addTo(map);
+      }
+    });
+}
+
+const pinMarkers = (map, countyName) => {
   function pinStations() {
     fetch('../../constants/elektrikli-sarj.json')
       .then(function (response) {
@@ -115,125 +169,6 @@ const KonyaElektrikIstasyonlari = () => {
   }
 
   pinStations();
-};
-
-const Cities = ({ setSelectedCity }) => {
-  const map = useMap();
-  function drawCityBoundary() {
-    fetch('../../constants/cities.geojson')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        const array = json.features;
-        for (let index = 0; index < array.length; index++) {
-          const element = array[index];
-
-          L.geoJSON(element.geometry, {
-            title: element?.properties?.name,
-            style: {
-              color: 'black',
-              fillColor: 'lighygray',
-              weight: 2,
-            },
-          })
-            .on('click', (e) => {
-              map.fitBounds(e.target.getBounds(), {
-                animate: true,
-              });
-              setSelectedCity(e?.target?.options?.title);
-              // drawCountyBoundary(map); // TODO: draw county boundaries
-            })
-            .on('mouseover', (e) => {
-              e.target.setStyle({
-                fillColor: 'red',
-              });
-            })
-            .on('mouseout', (e) => {
-              e.target.setStyle({
-                fillColor: 'lightgray',
-              });
-            })
-            .addTo(map);
-        }
-      });
-  }
-  drawCityBoundary();
-};
-
-function drawCountyBoundary(map) {
-  fetch('../../constants/konya_ilceler.geojson')
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (json) {
-      const array = json.features;
-      for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        L.geoJSON(element.geometry, {
-          style: {
-            color: 'lightgray',
-            weight: 2,
-          },
-        })
-          .on('click', (e) => {
-            map.fitBounds(e.target.getBounds(), {
-              animate: true,
-            });
-          })
-          .on('mouseover', (e) => {
-            e.target.setStyle({
-              color: 'red',
-            });
-          })
-          .on('mouseout', (e) => {
-            e.target.setStyle({
-              color: 'lightgray',
-            });
-          })
-          .addTo(map);
-      }
-    });
-}
-
-const Counties = () => {
-  const map = useMap();
-  function drawCountyBoundary() {
-    fetch('../../constants/konya_ilceler.geojson')
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (json) {
-        const array = json.features;
-        for (let index = 0; index < array.length; index++) {
-          const element = array[index];
-          L.geoJSON(element.geometry, {
-            style: {
-              color: 'black',
-              fillColor: 'lightgray',
-              weight: 2,
-            },
-          })
-            .on('click', (e) => {
-              map.fitBounds(e.target.getBounds(), {
-                animate: true,
-              });
-            })
-            .on('mouseover', (e) => {
-              e.target.setStyle({
-                fillColor: 'blue',
-              });
-            })
-            .on('mouseout', (e) => {
-              e.target.setStyle({
-                fillColor: 'lightgray',
-              });
-            })
-            .addTo(map);
-        }
-      });
-  }
-  drawCountyBoundary(name);
 };
 
 export default Map;
